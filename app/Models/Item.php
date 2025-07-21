bf317c7070c3edcc357c9f3f8944080404ce241b
Rafa <Rafael.EG.code@gmail.com>
2025-07-21 13:57:20 -0600

    Migraciones e instalación del paquete MediaLibrary

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Item extends Model
+class Item extends Model implements HasMedia
 {
+    use InteractsWithMedia;
     //
 }
--- app/Models/Vahul.php
+++ app/Models/Vahul.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Vahul extends Model
+class Vahul extends Model implements HasMedia
 {
-    //
+    use InteractsWithMedia;
 }
