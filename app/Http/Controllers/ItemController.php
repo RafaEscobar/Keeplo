8ebb70fd6788f51ba2dd48f49fbd813ac800cf60
Rafa <Rafael.EG.code@gmail.com>
2025-07-25 11:55:48 -0600

    Cargamos ItemStoreRequest

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2,9 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Store\ItemStoreRequest;
+use App\Http\Requests\Update\ItemUpdateRequest;
+use App\Http\Resources\Collections\ItemCollection;
+use App\Http\Resources\Resources\ItemResource;
+use App\Models\Item;
+use App\Models\Vahul;
 use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $requestValidated = $request->validate(['vahul_id' => 'required',], ['vahul_id.required' => 'El vahul asociado es requerido']);
+        $items = Item::where('vahul_id', $requestValidated['vahul_id'])->paginate($request->input('limit'));
+        return new ItemCollection($items);
+    }
+
+    public function store(ItemStoreRequest $request)
+    {
+        $vahul = Vahul::find($request->input('vahul_id'));
+        if ($vahul) {
+            $item = $vahul->items()->create($request->validated());
+            return new ItemResource($item);
+        } else {
+            return response()->json(["message" => "Vahul asociado no encontrado"]);
+        }
+    }
+
+    public function update(ItemUpdateRequest $request, Item $item)
+    {
+
+    }
+
+    public function show(Item $item)
+    {
+
+    }
+
+    public function destroy(Item $item)
+    {
+
+    }
 }
--- app/Http/Resources/Collections/ItemCollection.php
+++ app/Http/Resources/Collections/ItemCollection.php
@@ -2,18 +2,25 @@
 
 namespace App\Http\Resources\Collections;
 
+use App\Http\Resources\Resources\ItemResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class ItemCollection extends ResourceCollection
 {
-    /**
-     * Transform the resource collection into an array.
-     *
-     * @return array<int|string, mixed>
-     */
-    public function toArray(Request $request): array
+    public function toResponse($request)
     {
-        return parent::toArray($request);
+        return response()->json([
+            'data' => ItemResource::collection($this->collection),
+            'meta' => [
+                'total' => $this->collection->count(),
+                "links" => [
+                    'first' => $this->url(1),
+                    'last' => $this->url($this->lastPage()),
+                    'prev' => $this->previousPageUrl(),
+                    'next' => $this->nextPageUrl(),
+                ]
+            ]
+        ]);
     }
 }
